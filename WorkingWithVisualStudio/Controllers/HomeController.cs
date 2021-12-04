f7a6d9c248bd815d8cc72624d52b04d090eb97f9
DmitryDutov <dmitry_dutov@inbox.ru>
2021-12-04 21:57:35 +0300

    Все тесты пройдены

--- WorkingWithVisualStudio/Controllers/HomeController.cs
+++ WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace WorkingWithVisualStudio.Controllers
     {
         public IRepository Repository = SimpleRepository.SharedRepsitory;
         //SimpleRepository Repository = SimpleRepository.SharedRepsitory;
-        public IActionResult Index() => View(SimpleRepository.SharedRepsitory.Products.Where(p => p?.Price > 1));
+        public IActionResult Index() => View(Repository.Products);
 
         [HttpGet]
         public IActionResult AddProduct() => View(new Product());
